ef3c03dc69bcf41913c4075410c58f44d4807b49
islam <islammohammed2001@gmail.com>
2025-01-27 14:47:19 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans} from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const plusJakarta = Plus_Jakarta_Sans({
+  variable: "--font-jakarta-sans",
+  subsets: ['latin'],
+});
+
+
+export const metadata: Metadata = {
+  title: "Islam Farghl",
+  description: "I’m a front-end developer special in Nextjs frame work",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${cn(`${plusJakarta.variable} antialiased`)}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
